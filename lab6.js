43a886336472ce67d9e066f81eb6e6c66815770b
Christina Bergstrom <bergstromcbb@gmail.com>
2016-11-01 11:41:59 -0400

    updated lab 6 files

--- lab6.js
+++ lab6.js
@@ -1,13 +1,43 @@
 function convertTemp(degree, unit) {
 
-	if (unit==="C") {
-		return (5 / 9) * (degree - 32);
-	} else if (unit==="F") {
-		return (9 / 5 * degree) + 32;
-	}
+if (unit==="C" || unit==="c") {
+return (5 / 9) * (degree - 32);
+} else if (unit==="F" || unit==="f") {
+return (9 / 5 * degree) + 32;
+
+}
 }
 
-var convertedTemp = convertTemp(212, "C");
+var convertedTemp1 = convertTemp(212, "C"),
+    convertedTemp2 = convertTemp(32, "C"),
+    convertedTemp3 = convertTemp(65, "C"), 
+    convertedTemp4 = convertTemp(-40, "f")
 
-console.log(convertedTemp)
+
+
+console.log(convertedTemp1, convertedTemp2, convertedTemp3, convertedTemp4)
+
+
+
+
+
+// Option 2
+
+
+// function convertTemp(degree, unit) {
+
+// 	if (unit==="C") {
+// 		return (5 / 9) * (degree - 32);
+// 	} else if (unit==="F") {
+// 		return (9 / 5 * degree) + 32;
+// 	}
+// }
+//  var temp = 212
+
+//  var degreeUnit = "C"
+
+
+// var convertedTemp = convertTemp(temp, degreeUnit);
+
+// console.log(temp + degreeUnit + " converts to " +convertedTemp)
 
