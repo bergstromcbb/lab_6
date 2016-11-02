343ccbe2f218a84345d267e2fd2a988282a3fc40
Christina Bergstrom <bergstromcbb@gmail.com>
2016-11-02 09:21:49 -0400

    updated lab 6 files

--- lab6.js
+++ lab6.js
@@ -12,32 +12,13 @@ var convertedTemp1 = convertTemp(212, "C"),
     convertedTemp2 = convertTemp(32, "C"),
     convertedTemp3 = convertTemp(65, "C"), 
     convertedTemp4 = convertTemp(-40, "f")
+    convertedTemp5 = convertTemp(0, "f")
 
 
 
-console.log(convertedTemp1, convertedTemp2, convertedTemp3, convertedTemp4)
+console.log(convertedTemp1, convertedTemp2, convertedTemp3, convertedTemp4, convertedTemp5)
 
 
 
 
-
-// Option 2
-
-
-// function convertTemp(degree, unit) {
-
-// 	if (unit==="C") {
-// 		return (5 / 9) * (degree - 32);
-// 	} else if (unit==="F") {
-// 		return (9 / 5 * degree) + 32;
-// 	}
-// }
-//  var temp = 212
-
-//  var degreeUnit = "C"
-
-
-// var convertedTemp = convertTemp(temp, degreeUnit);
-
-// console.log(temp + degreeUnit + " converts to " +convertedTemp)
 
